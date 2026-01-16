3734c9ecdb0b1be23d5c81017474ccdeff7faec9
Samuel Ryberg <samuel.ryberg@gmail.com>
2026-01-16 09:58:38 +0100

    fixed some docker stuff to get stuff running again. Some edits still needed for backend and frontend to run correctly

--- frontend/app/components/products.jsx
+++ frontend/app/components/products.jsx
@@ -1,23 +1,21 @@
-import ProductCard from "./productcard"
+import ProductCard from "./productcard";
 
-export default async function Products(){
-    
-    const response = await fetch("http://backend:8080/products", {
-        cache: "no-store"
-    });
-    console.log(response);
-    
-    const data = await response.json();
-    const products = data;
+export default async function Products() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products``, {
+    cache: "no-store",
+  });
+  console.log(response);
 
-    console.log(products);
-    
-    return (
-        <div className="flex">
-        { 
-        products.map(product =>(   
-            <ProductCard key={product.id} product={product}/>
-        )) }
-        </div>
-        )
+  const data = await response.json();
+  const products = data;
+
+  console.log(products);
+
+  return (
+    <div className="flex">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
 }
